84ba933f4dd5b6ce348c19f861947773cefa2db7
paulocesarvasco <paulocvasco.93@gmail.com>
2025-03-26 12:57:21 -0300

    Fix functions to retreive database engine and session

--- app/services/database/db.py
+++ app/services/database/db.py
@@ -1,14 +1,15 @@
 import os
 from flask import g
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
 def get_db_engine():
     if 'db' not in g:
-        g.db = create_engine(os.getenv('DB_URL', ''), echo=True)
+        g.db = create_engine(os.getenv('DB_URL', ''), echo=False)
     return g.db
 
 
 def get_db_session():
-    engine = create_engine(os.getenv('DB_URL', ''), echo=True)
-    return Session(engine)
+    engine = create_engine(os.getenv('DB_URL', ''), echo=False)
+    Session = sessionmaker(bind=engine)
+    return Session()
