668f7afd5f66a44cb77f9d9781696cd9f2db58aa
paulocesarvasco <paulocvasco.93@gmail.com>
2025-03-28 19:01:48 -0300

    Define SearchResponsePayload as class model

---
+++ app/utils/models/payloads.py
@@ -0,0 +1,19 @@
+from flask import jsonify
+
+
+class SearchResponsePayload():
+
+    def __init__(self) -> None:
+        self.store_name = ''
+        self.purchase_date = ''
+        self.total = 0.0
+        self.purchases = []
+
+    def to_dict(self):
+        payload = dict()
+        payload['store_name'] = self.store_name
+        payload['purchase_date'] = self.purchase_date
+        payload['total'] = self.total
+        payload['purchases'] = self.purchases
+
+        return payload
