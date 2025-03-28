d4e30dfb183fb54a4607104b1aab9974856b4c95
paulocesarvasco <paulocvasco.93@gmail.com>
2025-03-28 20:03:23 -0300

    Finish implement of search view

--- app/api/search.py
+++ app/api/search.py
@@ -4,7 +4,11 @@ from flask import Blueprint, jsonify, request
 from app.services.database import db
 from app.utils.models import Product, PurchasedItem, ShoppingTrip
 from sqlalchemy import select
-from sqlalchemy.orm import joinedload, selectinload
+
+from app.utils.models.payloads import SearchResponsePayload
+
+import logging
+logger = logging.getLogger('views')
 
 searches_bp = Blueprint('searches', __name__, url_prefix='/search')
 
@@ -20,65 +24,63 @@ def search_purchases():
         with db.get_db_engine().connect() as conn:
             stmt = (
                 select(
+                    ShoppingTrip.store_name,
+                    ShoppingTrip.purchase_date,
+                    ShoppingTrip.total_amount,
+                    Product.product_name,
+                    PurchasedItem.brand,
+                    PurchasedItem.total_price,
+                    PurchasedItem.quantity
+                )
+                .select_from(
+                    PurchasedItem,
                     ShoppingTrip,
                     PurchasedItem
-                )
-                .join(
-                    PurchasedItem,
-                    ShoppingTrip.trip_id == PurchasedItem.trip_id
                 )
                 .where(
                     ShoppingTrip.purchase_date.between(start_date, end_date)
                 )
-                .order_by(
-                    ShoppingTrip.purchase_date.asc()
+                .where(
+                    ShoppingTrip.trip_id == PurchasedItem.trip_id,
+                )
+                .where(
+                    Product.product_id == PurchasedItem.product_id
                 )
             )
-            # stmt = (
-            #     select(ShoppingTrip)
-            #     .where(
-            #         ShoppingTrip.purchase_date.between(start_date, end_date)
-            #     )
-            #     .options(
-            #         selectinload(ShoppingTrip.items)  # This eagerly loads all related items
-            #     )
-            #     .order_by(
-            #         ShoppingTrip.purchase_date.desc()
-            #     )
-            # )
+            trips = conn.execute(stmt).all()
 
-            trips = conn.scalars(stmt).all()
-            # trips = session.scalars(stmt).unique().all()
+            committed_stores = dict()
+            for r in trips:
+                try:
+                    store_name, date, total, item, brand, price, quantity = r
+                    res = SearchResponsePayload()
+                    key_store = '{}+{}'.format(store_name, date)
+                    if key_store not in committed_stores.keys():
+                        res.store_name = store_name
+                        res.purchase_date = date
+                        res.total = total
+                        committed_stores['{}+{}'.format(store_name, date)] = res
+                    else:
+                        res = committed_stores['{}+{}'.format(store_name, date)]
 
-            print(trips)
+                    purchase = dict()
+                    purchase['brand'] = brand
+                    purchase['item'] = item
+                    purchase['price'] = price
+                    purchase['quantity'] = quantity
+                    res.purchases.append(purchase)
 
-        result = []
-        for trip in trips:
-            trip_data = {
-                "trip_id": trip.trip_id,
-                "store_name": trip.store_name,
-                "purchase_date": trip.purchase_date.isoformat(),
-                "total_amount": trip.total_amount,
-                "payment_method": trip.payment_method,
-                "items": []
-            }
+                except Exception as e:
+                    logger.error('failed to generate response payload')
+                    raise e
 
-            for item in trip.items:
-                item_data = {
-                    "item_id": item.item_id,
-                    "product_name": item.product.product_name,
-                    "category_name": item.product.category.category_name if item.product.category else None,
-                    "quantity": float(item.quantity),
-                    "unit_price": float(item.unit_price),
-                    "total_price": float(item.quantity * item.unit_price),
-                    "brand": item.brand,
-                    "is_on_sale": item.is_on_sale
-                }
-                trip_data["items"].append(item_data)
 
-            result.append(trip_data)
+            list_results = []
+            for _, data in committed_stores.items():
+                list_results.append(data.to_dict())
 
-        return jsonify(result)
+
+            return jsonify(list_results), HTTPStatus.OK
 
     except Exception as e:
         return jsonify({'error': str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
