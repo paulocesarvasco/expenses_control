1785b1f5fa754a8f5a31b28538306c072f03e1e1
paulocesarvasco <paulocvasco.93@gmail.com>
2025-03-31 13:42:42 -0300

    Centralize purchase logic at same file and blueprint

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,11 +1,10 @@
 from flask import Blueprint, render_template
-from .search import searches_bp
-from .register import registers_bp
+from .purchase import purchases_bp
 
 
 expenses_bp = Blueprint('expenses', __name__, url_prefix='/v1')
-expenses_bp.register_blueprint(registers_bp)
-expenses_bp.register_blueprint(searches_bp)
+expenses_bp.register_blueprint(purchases_bp)
+
 
 @expenses_bp.route('/')
 def root():
--- app/api/purchase.py
+++ app/api/purchase.py
@@ -6,17 +6,19 @@ from flask import Blueprint, render_template, request, jsonify
 from http import HTTPStatus
 from sqlalchemy.exc import SQLAlchemyError
 
+from app.utils.models.payloads import SearchResponsePayload
+
 import logging
 logger = logging.getLogger('views')
 
-registers_bp = Blueprint('registers', __name__, url_prefix='/register')
+purchases_bp = Blueprint('purchases', __name__, url_prefix='/purchase')
 
-@registers_bp.route('/forms')
+@purchases_bp.route('/register_form')
 def get_registry_form():
     return render_template('register.html')
 
 
-@registers_bp.route('/', methods=['POST'])
+@purchases_bp.route('/save', methods=['POST'])
 def save_purchase():
     s = Session()
     try:
@@ -76,3 +78,51 @@ def save_purchase():
     except Exception as e:
         s.rollback()
         return jsonify({'error': f'Unexpected error: {str(e)}'}), HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+@purchases_bp.route('/search')
+def search_purchases():
+    try:
+        start_date = request.args.get('start', type=str, default='2000-01-01')
+        end_date = request.args.get('end', type=str)
+
+        if end_date is None:
+            end_date = (datetime.now()).strftime('%Y-%m-%d')
+
+        trips = db.select_shopping_trips(start_date, end_date)
+        committed_stores = dict()
+        for trip in trips:
+            try:
+                store_name, date, total, item, brand, price, quantity = trip
+                res = SearchResponsePayload()
+                key_store = '{}+{}'.format(store_name, date)
+                if key_store not in committed_stores.keys():
+                    res.store_name = store_name
+                    res.purchase_date = date
+                    res.total = total
+                    committed_stores['{}+{}'.format(store_name, date)] = res
+                else:
+                    res = committed_stores['{}+{}'.format(store_name, date)]
+
+                purchase = dict()
+                purchase['brand'] = brand
+                purchase['item'] = item
+                purchase['price'] = price
+                purchase['quantity'] = quantity
+                res.purchases.append(purchase)
+
+            except Exception as e:
+                logger.error('failed to generate response payload')
+                raise e
+
+
+        list_results = []
+        for _, data in committed_stores.items():
+            list_results.append(data.to_dict())
+
+
+        # return jsonify(list_results), HTTPStatus.OK
+        return render_template('search.html', data=list_results)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
